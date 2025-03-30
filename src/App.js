ed58cfed972c6c42017e3d2535f7a62f46675b3d
Diksha Malviya <dikshamalviya@Dikshas-Laptop.local>
2025-03-30 16:55:50 +0530

    home,bolg,aboutus

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
-import carImage from "./image.png";
 import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import Demo from "./demo"; 
 import Home from "./home"; 
+import AboutUs from "./about"; 
+import Blog from "./blog"; 
 function App() {
   return (
-    <div className="App">
-    
-      <header className="App-header">
-     <div className='container'>
-    <img src={carImage} alt="Image" className='right-img' />
-    </div>
-    <div className='left-div'>
-    <h1 style={{ fontSize: "52px"}}>Welcome to Travel Tails</h1>
-    <p style={{ fontSize: "18px" }}> Plan and book your perfect trip with expert advice, <br/>
-
-    travel tips, destination information and inspiration from us.</p>
-    <a href="/home" className="custom-link">Go to Home</a>
-      </div>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Demo />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/about" element={<AboutUs />} />
+        <Route path="/blog" element={<Blog />} />
+      </Routes>
+    </Router>
   );
 }
 
--- src/home.js
+++ src/home.js
@@ -1,7 +1,31 @@
 import React from "react";
-
+import "./Header.css";
+import Icon from "./icon.jpeg";
+import ReactPlayer from "react-player";
+import carimg from "./homecar.png";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 function Home() {
-  return <h1>Welcome to the Home Page</h1>;
+  return(
+    <div>
+   <header className="header">
+      {/* Left Side Icon and Logo */}
+      <div className="logo-container">
+      <img src={Icon} className="icon"/>
+        <h1 className="logo-text">Travel Trail</h1>
+      </div>
+
+      {/* Navigation Links */}
+      <nav className="nav-links">
+        <Link to="/home" className="nav-link">Home</Link>
+        <Link to="/blog" className="nav-link">Blog</Link>
+        <Link to="/about" className="nav-link">About Us</Link>
+      </nav>
+    </header>
+  
+
+    </div>
+
+  ); 
 }
 
 export default Home;
