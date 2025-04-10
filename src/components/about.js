71bd13983b040d1b6f3037ba8e00444c83614349
Diksha Malviya <dikshamalviya@Dikshas-Laptop.local>
2025-04-10 19:34:32 +0530

    about-us

--- src/components/about.js
+++ src/components/about.js
@@ -3,6 +3,7 @@ import "./../css/Header.css";
 import Icon from "./../images/icon.jpeg";
 import car1 from "./../images/car1.png";
 import car2 from "./../images/car2.png";
+import taxi from "./../images/taxi1.png";
 import "./../css/About.css";
 import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 function About() {
@@ -25,28 +26,40 @@ function About() {
     </header>
 </div>
   
-  
- 
-  
-  <div style={{ display: 'flex', gap: 5,marginTop:110, width:300 , marginLeft: 500 }}>
+  <div style={{ display: 'flex', gap: 5,marginTop:29, width:300 , marginLeft: 500 }}>
   <div className="card-container"> 
       <div className="card">
-        <h2>This is a Card</h2>
-        <p>You can write your text here inside the card.</p>
+        <h3 style={{ color: 'white' }}>WHEREVER YOU GO, WE’LL DRIVE YOU THERE 😉</h3>
+        <h6 style={{ color: 'brown' }}>Fast, safe, and reliable cab bookings at your fingertips.</h6>
+        <p>Book a ride in seconds and enjoy seamless travel—anytime, anywhere.</p>
+
       </div>
     </div>
       <div >
-      <img src={car2} alt="Image" style={{ width: 200, height:250 , marginLeft:5, marginTop:25, borderRadius: 16}}/>
+      <img src={car2} alt="Image" style={{ width: 250, height:250 , marginLeft:256, marginTop:25, borderRadius: 16}}/>
       </div>
     </div>
     <div style={{ display: 'flex', gap: 5, width:300 , marginLeft: 500 }}>
       <div style={{  }}>
-      <img src={car1} alt="Image" style={{ width: 200 , height:250, borderRadius: 16}} />
+      <img src={car1} alt="Image" style={{ width: 250 , height:250, borderRadius: 16}} />
       </div>
       <div style={{ }}>
-      <img src={car2} alt="Image" style={{ width: 200, height:250, borderRadius: 16}}/>
+      <img src={taxi} alt="Image" style={{ width: 250, height:250, borderRadius: 16}}/>
       </div>
       
+    </div>
+    <div>
+      <h1 style={{textAlign:'center'}}>ABOUT US</h1>
+      <p style={{ color: 'brown', textAlign:'center' }}>'RIDES THAT MATCH YOUR TRAVEL STYLE'</p>
+      <p style={{ color: 'black', textAlign:'center' }}>Welcome to Travel Trail, your trusted travel partner!
+We specialize in hassle-free cab bookings and personalized trip planning.
+<br/>
+Whether you’re commuting across the city, heading to the airport, or planning a weekend escape—we’re here to make your journey smooth, safe, and reliable.
+<br/>
+With a user-friendly booking system, 24/7 customer support, and a wide fleet of vehicles, we bring convenience to your fingertips.
+<br/>
+Join thousands of happy travelers who choose us every day.<br/>
+Wherever you go—we’ll get you there.</p>
     </div>
     </div>
 
